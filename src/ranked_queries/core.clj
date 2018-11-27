d1d8312555ef012a63443ac5001d65623894b262
Jelmer de Ronde <jelmerderonde@gmail.com>
2018-11-27 11:41:18 +0100

    Adds .webloc files

--- src/ranked_queries/core.clj
+++ src/ranked_queries/core.clj
@@ -4,7 +4,8 @@
             [clojure.string :as str]
             [cheshire.core :as json]
             [clj-http.client :as http]
-            [digest :as digest]))
+            [digest :as digest]
+            [clojure.data.xml :as xml]))
 
 (defn load-items
   [filename]
@@ -15,23 +16,43 @@
 
 (def items (load-items "elasticsearch-documents.json"))
 
+(defn write-webloc
+  [url path]
+  (let [tags (xml/sexp-as-element
+               ["plist" {"version" "1.0"}
+                ["dict"
+                 ["key" "URL"]
+                 ["string" url]]])]
+    (with-open [w (java.io.FileWriter. path)]
+      (xml/emit tags w))))
+
 (defn get-filename
   [headers]
   (->> (get headers "Content-Disposition")
        (re-matches #"attachment; filename=\"(.+)\";")
        last))
 
+(defn clean-filename
+  [filename]
+  (str/replace filename #"[*&%/]+" ""))
+
 (defn download-item
-  [{:keys [item-url]}]
-  (let [res (http/get item-url {:insecure? true
-                                :as :byte-array})
-        filename (get-filename (:headers res))
-        sha1 (digest/sha-1 (:body res))
-        path (str "output/" sha1 " - " filename)]
-    (with-open [w (io/output-stream path)]
-      (.write w (:body res)))))
+  [{:keys [item-url title] :as item}]
+  (let [{:keys [headers body]} (http/get item-url {:insecure? true
+                                                   :as :byte-array})
+        folder "output/"
+        sha1 (digest/sha-1 item-url)]
+    {:item (dissoc item :text)
+     :headers headers
+     :result
+     (if (contains? headers "Content-Disposition")
+       (let [filename (get-filename headers)
+             path (str folder sha1 " - " filename)]
+         (with-open [w (io/output-stream path)]
+           (.write w body)))
+       (let [filename (clean-filename (str title ".webloc"))
+             path (str folder sha1 " - " filename)]
+         (write-webloc item-url path)))}))
 
-(download-item (first items))
-
-
-
+(comment
+  (map download-item items))
