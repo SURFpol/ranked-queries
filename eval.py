65a67fc14c14829df0ad81f1735c46fc3f93fd45
Matthijs Moed <matthijs.moed@surfsara.nl>
2018-12-13 16:23:24 +0100

    Made evaluation script a bit more flexible.

--- eval.py
+++ eval.py
@@ -1,22 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import json
 import re
 from itertools import chain
 
 import click
 import requests
+from toolz.dicttoolz import assoc
 
 
 ENDPOINT = "https://surfpol.sda.surf-hosted.nl"
 USERNAME = ""
 PASSWORD = ""
-INDEX = "freeze-1"
-
-ES_METRIC = {
+METRICS = {
     'precision': {
-        'k': 20,
         'relevant_rating_threshold': 1,
         'ignore_unlabeled': False           
+    },
+    'mean_reciprocal_rank': {
+        'relevant_rating_threshold': 1
+    },
+    'dcg': {
+        'normalize': True
+    },
+    'expected_reciprocal_rank': {
+        'maximum_relevance': 5
     }
 }
 
@@ -26,15 +33,11 @@ def load_json(path):
         return json.load(stream)
 
 
-def format_multi_match(query_text):
+def format_multi_match(query_text, fields):
     return [
         {
             "multi_match": {
-                "fields": [
-                    "text.en",
-                    "text.nl",
-                    "title"
-                ],
+                "fields": fields,
                 "fuzziness": 0,
                 "operator": "or",
                 "query": query_text,
@@ -43,11 +46,7 @@ def format_multi_match(query_text):
         },
         {
             "multi_match": {
-                "fields": [
-                    "text.en",
-                    "text.nl",
-                    "title"
-                ],
+                "fields": fields,
                 "operator": "or",
                 "query": query_text,
                 "type": "phrase_prefix"
@@ -56,7 +55,7 @@ def format_multi_match(query_text):
     ]
 
 
-def format_requests(query):
+def format_requests(query, index, fields):
     requests = []
     for item in query['queries']:
         request = {
@@ -65,12 +64,12 @@ def format_requests(query):
                 'query': {
                     'bool': {
                         'minimum_should_match': 1,
-                        'should': format_multi_match(item)
+                        'should': format_multi_match(item, fields)
                     }
                 }   
             },
             'ratings': [
-                {'_index': INDEX, '_id': doc['hash'], 'rating': doc['rating']}
+                {'_index': index, '_id': doc['hash'], 'rating': doc['rating']}
                 for doc in query['items']
             ]
         }
@@ -78,15 +77,24 @@ def format_requests(query):
     return requests
 
 
+def get_metric(name, k):
+    return {name: assoc(METRICS[name], 'k', k)}
+
+
 @click.command()
 @click.argument('queries', type=load_json)
-def main(queries):
+@click.argument('index')
+@click.argument('metric', type=click.Choice(METRICS.keys()))
+@click.option('--field', multiple=True, default=['title', 'text.nl', 'text.en'])
+@click.option('-k', type=int, default=20, help='max. #documents per query')
+def main(queries, index, metric, k, field):
     body = {
-        'requests': list(chain(*[format_requests(_) for _ in queries])),
-        'metric': ES_METRIC
+        'requests': list(chain(*[format_requests(_, index, field) for _ in queries])),
+        'metric': get_metric(metric, k)
     }
+
     result = requests.get(
-        '{}/{}/_rank_eval'.format(ENDPOINT, INDEX),
+        '{}/{}/_rank_eval'.format(ENDPOINT, index),
         auth=(USERNAME, PASSWORD),
         json=body
     )
